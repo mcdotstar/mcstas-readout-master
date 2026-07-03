bf8a1639d34e4eec8db0b597912563eb1d062677
Gregory Tucker <gregory.tucker@ess.eu>
2026-07-03 09:23:38 +0200

    [Fix] refactor HighFive dependency to PRIVATE

--- cmake/readout-hdf5.cmake
+++ cmake/readout-hdf5.cmake
@@ -1,9 +1,5 @@
 # HighFive is used to simplify the interface with HDF5:
 find_package(HighFive REQUIRED)
 foreach(HF_TARGET IN LISTS CXX_TARGETS)
-    target_link_libraries(${HF_TARGET} PUBLIC
-            $<BUILD_INTERFACE:HighFive>
-            $<INSTALL_INTERFACE:>
-    #        HighFive
-    )
+    target_link_libraries(${HF_TARGET} PRIVATE HighFive)
 endforeach()
