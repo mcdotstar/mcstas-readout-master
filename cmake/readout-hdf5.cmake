68d0fef1fa2c6994a3f5341340c4fa438a9b70ea
Gregory Tucker <gregory.tucker@ess.eu>
2026-07-03 09:24:47 +0200

    [Fix] but keep the headers visible for building

--- cmake/readout-hdf5.cmake
+++ cmake/readout-hdf5.cmake
@@ -1,5 +1,12 @@
 # HighFive is used to simplify the interface with HDF5:
 find_package(HighFive REQUIRED)
+
+# Public C++ headers (e.g. hdf_interface.h) include HighFive headers, so
+# build-tree consumers of Readout::readout need include visibility.
+target_include_directories(${READOUT_LIBRARY_TARGET} PUBLIC
+        $<BUILD_INTERFACE:$<TARGET_PROPERTY:HighFive,INTERFACE_INCLUDE_DIRECTORIES>>
+)
+
 foreach(HF_TARGET IN LISTS CXX_TARGETS)
     target_link_libraries(${HF_TARGET} PRIVATE HighFive)
 endforeach()
